96122c4d1328aaf1ba169b9b29366265d0b10e26
hnepla <henrylin95@gmail.com>
2021-08-28 20:35:57 +1000

    first commit

---
+++ summaries_generator.py
@@ -0,0 +1,35 @@
+import docx
+import os
+
+# take in name of subject, & topics as a list
+teaching_period = "21T4"
+subject = "Applied Data Science"
+topics = [
+    "Data sources",
+    "Noisy data & reliability",
+    "Analysis & verification",
+    "Visualisation",
+    "Validation",
+    "Presenting results for decision-making",
+    "Revision",
+]
+
+
+# create a new folder with the name of subject
+current_dir = os.getcwd()
+
+final_dir = os.path.join(current_dir, rf"{teaching_period} {subject}")
+
+if not os.path.exists(final_dir):
+    os.makedirs(final_dir)
+    print("Error: folder already exists")
+
+
+# loop and title each topic as title formatting
+for i in range(len(topics)):
+    doc = docx.Document()
+
+    doc_title = f"{i + 1} {topics[i]}"
+    doc.add_paragraph(doc_title, "Title")
+
+    doc.save(f"{final_dir}\\" + f"{doc_title}.docx")
